ce4752a040bfa60b019f406df1d9a2f3ffc0da3c
Chesla <chesla@happay.in>
2016-09-22 23:03:55 +0530

    counter changed

--- my-phase/src/Counter.js
+++ my-phase/src/Counter.js
@@ -1,8 +1,29 @@
 import React from 'react';
- 
+import Snackbar from 'material-ui/Snackbar';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/action/reorder';
+import IconMenu from 'material-ui/IconMenu';
+import Menu from 'material-ui/Menu/Menu';
+import MenuItem from 'material-ui/MenuItem/MenuItem';
+import CircularProgress from 'material-ui/CircularProgress';
+import Paper from 'material-ui/Paper';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 /**
  * A counter button: tap the button to increase the count.
  */
+ const styles = {
+  exampleImageInput: {
+    cursor: 'pointer',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    width: '100%',
+    opacity: 0,
+  },
+};
 class Counter extends React.Component {
   constructor() {
     super();
@@ -13,13 +34,38 @@ class Counter extends React.Component {
  
   render() {
     return (
-      <button
-        onClick={() => {
-          this.setState({ count: this.state.count + 1 });
-        }}
-      >
-        Count: {this.state.count}
-      </button>
+      <Card>
+        <CardHeader
+          title="URL Avatar"
+          subtitle="Subtitle"
+          avatar="images/jsa-128.jpg"
+        />
+        <CardMedia
+          overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+        >
+          <img src="images/nature-600-337.jpg" />
+        </CardMedia>
+        <CardTitle title="Card title" subtitle="Card subtitle" />
+        <CardText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+        <CardActions>
+           <FlatButton
+              label="Count:"
+              labelPosition="before"
+              primary={true}
+              style={styles.exampleImageInput}
+              onClick={() => {
+                this.setState({ count: this.state.count + 1 });
+              }}
+              icon={<ActionAndroid />}
+            />{this.state.count}
+        </CardActions>
+      </Card>
+      
     );
   }
 }
